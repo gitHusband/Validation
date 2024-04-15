22da53f7e36e24bc76a2eb6014eec96038555f67
gitHusband <707077549@qq.com>
2024-04-15 23:14:38 +0800

    #Test Set LOG_LEVEL from cli for multi-test

--- src/Test/Test.sh
+++ src/Test/Test.sh
@@ -7,6 +7,13 @@ else
     exit 1
 fi
 
+if [ -n "$VALIDATION_LOG_LEVEL" ]
+then
+   env_log_level="-e VALIDATION_LOG_LEVEL=$VALIDATION_LOG_LEVEL"
+else
+   env_log_level=""
+fi
+
 method=$1;
 sub_method_1=$2;
 sub_method_2=$3;
@@ -16,7 +23,7 @@ scriptDirectory=$(dirname "$scriptPath")
 
 echo ">> Test in docker container: php-5.6-cli-alpine";
 # docker run -i --rm --name php-5.6-cli-alpine -v "$PWD":/validation -w /validation/src/Test php:5.6-cli-alpine php Test.php Unit run
-docker run -i --rm --name php-5.6-cli-alpine -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:5.6-cli-alpine php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
+docker run -i --rm $env_log_level --name php-5.6-cli-alpine -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:5.6-cli-alpine php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
 returnValue=$?
 if [ $returnValue -ne 0 ]; then
     exit 1
@@ -26,7 +33,7 @@ echo "";
 
 echo ">> Test in docker container: php-7.4.33-cli-alpine";
 # docker run -i --rm --name php-7.4.33-cli-alpine -v "$PWD":/validation -w /validation/src/Test php:7.4.33-cli-alpine php Test.php Unit run
-docker run -i --rm --name php-7.4.33-cli-alpine -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:7.4.33-cli-alpine php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
+docker run -i --rm $env_log_level --name php-7.4.33-cli-alpine -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:7.4.33-cli-alpine php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
 returnValue=$?
 if [ $returnValue -ne 0 ]; then
     exit 1
@@ -35,7 +42,7 @@ echo "";
 
 echo ">> Test in docker container: php-latest";
 # docker run -i --rm --name php-latest -v "$PWD":/validation -w /validation/src/Test php:latest php Test.php Unit run
-docker run -i --rm --name php-latest -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:latest php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
+docker run -i --rm $env_log_level --name php-latest -v "$scriptDirectory"/../../:/validation -w /validation/src/Test php:latest php Test.php Unit "$method" "$sub_method_1" "$sub_method_2"
 returnValue=$?
 if [ $returnValue -ne 0 ]; then
     exit 1
